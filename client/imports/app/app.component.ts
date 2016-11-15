bc5566ac73bef21a8a23ba6bb97a2bf0ae05d589
Cthurs <cyh0123q@gmail.com>
2016-11-15 22:38:39 +0900

    add home temporarily data

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import template from './app.component.html';
 
 @Component({
-  selector: 'app',
-  template
+    selector: 'app',
+    template: `
+        <ul>
+            <li *ngFor="let home of homes"
+                (click)="onSelect(home)"
+                [class.clicked]="selectedHome===home">
+                {{home.hoster}}의 집
+            </li>
+        </ul>
+        <div>
+            위치 : {{selectedHome.location}}<br>
+            가격 : {{selectedHome.price}}
+        </div>
+  `,
+    styleUrls: ["../../app.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+    public homes = [
+        {hoster: "Cha YeongHun", location: "busan", price: "300$"},
+        {hoster: "Chris", location: "paris", price: "299$"},
+        {hoster: "Michael", location: "NY", price: "350$"}
+    ]
+    public selectedHome = {};
+    onSelect(home) {
+        this.selectedHome = home;
+    }
+}
