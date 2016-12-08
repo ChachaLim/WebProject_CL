1adddef4b99baa4366a886df3615b153f2275169
Cthurs <cyh0123q@gmail.com>
2016-12-09 05:17:05 +0900

    호스터 기능 추가 (업데이트, 추가, 제거)

--- ng2_ccl_project/src/app/af.component.ts
+++ ng2_ccl_project/src/app/af.component.ts
@@ -4,20 +4,38 @@ import {FirebaseListObservable, AngularFire} from "angularfire2";
 @Component({
   selector: 'af',
   template: `
-    <p>firebase 연동 부분</p>
+    <p>호스터 기능</p>
     <ul>
         <li *ngFor="let item of items | async"
             (click)="onSelect(item)"
             >
-            {{item.name}}
-            {{item.$value}}
+            {{item.hoster}}의 {{item.name}}
+            <!--{{item.$value}}-->
+            <button (click)="delete(item.$key)">x</button>
+            
         </li>
     </ul>
     <div *ngIf="selectedItem">
-    <input type="text" [(ngModel)]="selectedItem.name">
+    <label>주택 업데이트</label><br>
+      <label>house name</label><input type="text" [(ngModel)]="selectedItem.name"><br>
+      <label>hoster name</label><input type="text" [(ngModel)]="selectedItem.hoster"><br>
+      <label>house lat</label><input type="text" [(ngModel)]="selectedItem.lat"><br>
+      <label>house lng</label><input type="text" [(ngModel)]="selectedItem.lng">
+      <button (click)="update(selectedItem.$key,
+                             selectedItem.name, 
+                             selectedItem.hoster,
+                             selectedItem.lat,
+                             selectedItem.lng)">Update</button>
+      <br />
     </div>
-    <br />
-    <button (click)="delete()">Delete</button>
+    <br>
+    
+    <label>주택 추가</label><br>
+    <input type="text" [(ngModel)]="name" placeholder="house name"><br>
+    <input type="text" [(ngModel)]="hoster" placeholder="hoster name"><br>
+    <input type="text" [(ngModel)]="lat" placeholder="latitude"><br>
+    <input type="text" [(ngModel)]="lng" placeholder="lngitude">
+    <button (click)="newHouse(name, hoster, lat, lng)">Add</button>
     <p>----------------------</p>
   `
 })
@@ -35,7 +53,14 @@ export class AfApp {
     console.log(this.selectedItem.price);
   }
 
-  delete() {
-    this.items.remove('/4');
+  newHouse(newName, newHoster, newLat, newLng): void {
+    this.items.push({name: newName, hoster: newHoster, lat: Number(newLat), lng: Number(newLng)});
+  }
+
+  delete(key) {
+    this.items.remove(key);
+  }
+  update(key, newName, newHoster, newLat, newLng){
+    this.items.update(key, {name:newName, hoster:newHoster, lat:Number(newLat), lng:Number(newLng)});
   }
 }
