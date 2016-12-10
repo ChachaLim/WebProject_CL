4c736892b322516d8ac9f73f77b9a8b67a73c508
임종식 <whdtlr1218@gmail.com>
2016-12-10 20:26:46 +0900

    add filter pipe

--- ng2_ccl_project/src/app/filter.pipe.ts
+++ ng2_ccl_project/src/app/filter.pipe.ts
@@ -5,13 +5,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(houses: any, term: any): any {
+  transform(houses: any, term: any, opt: any): any {
     //check if search term is undefined
-    if(term === undefined) return houses;
-    //return updated hosese array
-    return houses.filter(function(house){
-      return house.hoster.toLowerCase().includes(term.toLowerCase());
-    })
+    if(opt==="hoster"){
+      if(term === undefined) return houses;
+      //return updated hosese array
+      console.log("Pipe_hoster");
+      console.log(houses);
+      return houses.filter(function(house){
+        return house.hoster.toLowerCase().includes(term.toLowerCase());
+      })
+    }
+    else if(opt==="place"){
+      if(term === undefined) return houses;
+      //return updated hosese array
+      console.log("Pipe_place");
+      console.log(houses);
+      return houses.filter(function(house){
+        return house.place.toLowerCase().includes(term.toLowerCase());
+      })
+    }
+
   }
 
 }
